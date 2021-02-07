cc26684ef3f9ac03d1fbf56c41bcc579e069a408
Kapil <42500918+kapilkumaria@users.noreply.github.com>
2021-02-07 11:52:00 -0700

    Create dockerpermission.sh

---
+++ dockerpermission.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo su 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 931058976119.dkr.ecr.us-east-1.amazonaws.com
+docker build -t my-nodeapp .
+docker tag my-nodeapp:latest 931058976119.dkr.ecr.us-east-1.amazonaws.com/my-nodeapp:latest
+docker push 931058976119.dkr.ecr.us-east-1.amazonaws.com/my-nodeapp:latest
