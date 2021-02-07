8b72e7d9bc71553b12be278ecaa38941969de3f9
Kapil <42500918+kapilkumaria@users.noreply.github.com>
2021-02-07 12:58:59 -0700

    Update dockerpermission.sh

--- dockerpermission.sh
+++ dockerpermission.sh
@@ -4,4 +4,4 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker build -t my-nodeapp .
 docker tag my-nodeapp:latest 931058976119.dkr.ecr.us-east-1.amazonaws.com/my-nodeapp:latest
 docker push 931058976119.dkr.ecr.us-east-1.amazonaws.com/my-nodeapp:latest
-echo "Helloo WORLDS!"
+echo "HElloo WORLDS!"
